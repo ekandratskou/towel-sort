8d272596ec8de3bb6baca36d79bd070738522909
Eduard <edyard2002@mail.ru>
2026-05-06 00:20:51 +0300

    feat:add the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const res = [];
+  if (!matrix || matrix.length === 0) return res;
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 !== 0) {
+      res.push(matrix[i].reverse());
+    } else {
+      res.push(matrix[i]);
+    }
+  }
+  return res.flat();
 };
